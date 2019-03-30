9180209ddac9a9e609bdc303414270047d618034
seongahjo <seongside@gmail.com>
2019-03-31 04:47:09 +0900

    hotfix: add configuration annotation

--- src/main/java/com/sajo/study/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/sajo/study/configuration/WebSecurityConfiguration.java
@@ -5,6 +5,7 @@ import com.sajo.study.security.SecurityContextRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
@@ -12,6 +13,8 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import reactor.core.publisher.Mono;
 
+
+@Configuration
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 @Slf4j
